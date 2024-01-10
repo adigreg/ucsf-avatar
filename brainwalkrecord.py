f8d11dc907b45bde9335937a27fa21433a0697e5
adigreg <ariannadigregorio2020@u.northwestern.edu>
2024-01-10 08:30:31 -0800

    Push SVG edits into BrainWalkData class

--- brainwalkrecord.py
+++ brainwalkrecord.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, division, print_function
 from enum import Enum
-"""Colorize the SVG by modifying the XML directly, via ElementTree."""
-import random
-import sys
 from xml.etree import ElementTree as ET
 import os 
-import yaml
+import logging
 
 BODY_PART_TO_BRAINWALK_FIELDS = {"arm_right":["right_arm","strength_rt_arm","spasm_rt_arm","tremor_arms"],
         "arm_left":["left_arm","strength_lt_arm","spasm_lt_arm","tremor_arms"],
@@ -17,99 +14,74 @@ BODY_PART_TO_BRAINWALK_FIELDS = {"arm_right":["right_arm","strength_rt_arm","spa
         "face_right":["rt_face","feeling_rt"],
         "face_left":["lt_face","feeling_lt"],
         "abdomen":["bowel_bladder_score"],
-        "brain": ["cognition","mfis_fatigue"],
+        "brain": ["cognition","fatigue"],
         "mouth":["speak"],
         "neck":["swallow"],
         "ear_left":["hearing"],
         "ear_right":["hearing"]}
 
-BRAINWALK_FIELD_STRING_TO_INT_SCORE = {"speak": {"":0, "I do not have problems speaking":0, "I sometimes slur words but others don't seem to notice":1, "I often slur words and others notice":2, "I slur words so much that it interferes with my ability to have conversations":3, "I slur my words so much that others cannot understand me":4, "I cannot speak":5},
-        "swallow": {"":0, "I do not have any problems swallowing liquids or foods":0, "I have problems swallowing liquids or solid foods":1, "I have frequent problems with swallowing and need a pureed diet":2, "I cannot swallow food or liquids":3},
-        "hearing": {"":0, "I do not have problems hearing":0, "I have mild hearing loss on one side":1, "I have moderate or severe hearing loss on one side":2, "I have total hearing loss on both sides. I am effectively deaf.":3}, 
+BRAINWALK_FIELD_STRING_TO_INT_SCORE = {"speak": {"I do not have problems speaking":0, "I sometimes slur words but others don't seem to notice":1, "I often slur words and others notice":2, "I slur words so much that it interferes with my ability to have conversations":3, "I slur my words so much that others cannot understand me":4, "I cannot speak":5},
+        "swallow": {"I do not have any problems swallowing liquids or foods":0, "I have problems swallowing liquids or solid foods":1, "I have frequent problems with swallowing and need a pureed diet":2, "I cannot swallow food or liquids":3},
+        "hearing": {"I do not have problems hearing":0, "I have mild hearing loss on one side":1, "I have moderate or severe hearing loss on one side":2, "I have total hearing loss on both sides. I am effectively deaf.":3}, 
         "cognition": {"I have no problems with concentration or memory":0, "I have some concentration and memory problems, or problems with coping with stress, but I am able to handle my daily routines including completing this survey":1, "I have problems with concentration and memory that my friends and family notice; this is beginning to affect my daily routine. It makes completing this survey difficult":2, "I have severe impairment in my cognitive (thinking) abilities; for example, I sometimes forget where I am and who I am talking to. I need help completing this survey":3, "I have no meaningful conversation and am unable to handle my affairs because of my severe cognitive problems; I need someone else to complete this survey":4},
-        "rt_face": {"":0,"I do not have muscle weakness in my face":0, "A little, such as when I furrow my eyebrows or laugh":1,"A lot, such as trouble with drooling or when closing my eye(s)":2,"Total weakness or palsy, such as Bell's palsy":3},
-        "lt_face": {"":0,"I do not have muscle weakness in my face":0, "A little, such as when I furrow my eyebrows or laugh":1,"A lot, such as trouble with drooling or when closing my eye(s)":2,"Total weakness or palsy, such as Bell's palsy":3},
-        "feeling_rt": {"":0, "Feeling is very good. No numbness or pain":0, "Feeling is good. Some numbness":1, "Feeling is fair. Hard to tell sharp touch from dull touch":2, "Feeling is poor. Mild pain":3, "Feeling is very poor or gone. Moderate to severe pain":4},
-        "feeling_lt": {"":0, "Feeling is very good. No numbness or pain":0, "Feeling is good. Some numbness":1, "Feeling is fair. Hard to tell sharp touch from dull touch":2, "Feeling is poor. Mild pain":3, "Feeling is very poor or gone. Moderate to severe pain":4},
-        "vision_rt": {"":0, "I have no problems with my ability to see.":0, "My vision is slightly impaired":1, "My vision is very impaired":2, "I am blind, or almost blind, in that eye":3},
-        "vision_lt": {"":0, "I have no problems with my ability to see.":0, "My vision is slightly impaired":1, "My vision is very impaired":2, "I am blind, or almost blind, in that eye":3},
-        "right_arm": {"":0,"Feeling is very good - No problems": 0, "Feeling is mildly impaired": 1, "Feeling is clearly impaired": 2, "Feeling is very poor or completely gone": 3},
-        "right_leg": {"":0,"Feeling is very good - No problems": 0, "Feeling is mildly impaired": 1, "Feeling is clearly impaired": 2, "Feeling is very poor or completely gone": 3},
-        "left_arm": {"":0,"Feeling is very good - No problems": 0, "Feeling is mildly impaired": 1, "Feeling is clearly impaired": 2, "Feeling is very poor or completely gone": 3},
-        "left_leg": {"":0,"Feeling is very good - No problems": 0, "Feeling is mildly impaired": 1, "Feeling is clearly impaired": 2, "Feeling is very poor or completely gone": 3},
-        "strength_rt_arm": {"":0,"I can easily raise it and keep it raised":0, "I can raise it, but have mild or occasional trouble with my full strength": 1, "I have to make some effort to raise it": 2, "I can barely raise it": 3, "I can move my limb, but I cannot raise it": 4, "I cannot move it at all":5},
-        "strength_lt_arm": {"":0,"I can easily raise it and keep it raised":0, "I can raise it, but have mild or occasional trouble with my full strength": 1, "I have to make some effort to raise it": 2, "I can barely raise it": 3, "I can move my limb, but I cannot raise it": 4, "I cannot move it at all":5},
-        "strength_lt_leg": {"":0,"I can easily raise it and keep it raised":0, "I can raise it, but have mild or occasional trouble with my full strength": 1, "I have to make some effort to raise it": 2, "I can barely raise it": 3, "I can move my limb, but I cannot raise it": 4, "I cannot move it at all":5},
-        "strength_rt_leg": {"":0,"I can easily raise it and keep it raised":0, "I can raise it, but have mild or occasional trouble with my full strength": 1, "I have to make some effort to raise it": 2, "I can barely raise it": 3, "I can move my limb, but I cannot raise it": 4, "I cannot move it at all":5},
-        "tremor_legs": {"":0,"I do not have tremors or coordination problems":0, "Rarely make it hard for me to use":1, "Sometimes make it hard for me to use":2, "Often make it hard for me to use":3, "Always make it hard for me to use":4},
-        "tremor_arms": {"":0,"I do not have tremors or coordination problems":0, "Rarely make it hard for me to use":1, "Sometimes make it hard for me to use":2, "Often make it hard for me to use":3, "Always make it hard for me to use":4},
-        "spasm_rt_arm": {"":0,"I do not have stiffness or spasms":0, "Mild, does not make it hard for me to use":1, "Moderate stiffness, but with effort I can use":2, "Sometimes I cannot overcome the stiffness to use my arm or leg":3, "My arm or leg is so contracted that I cannot use it at all":4},
-        "spasm_lt_arm": {"":0,"I do not have stiffness or spasms":0, "Mild, does not make it hard for me to use":1, "Moderate stiffness, but with effort I can use":2, "Sometimes I cannot overcome the stiffness to use my arm or leg":3, "My arm or leg is so contracted that I cannot use it at all":4},
-        "spasm_rt_leg": {"":0,"I do not have stiffness or spasms":0, "Mild, does not make it hard for me to use":1, "Moderate stiffness, but with effort I can use":2, "Sometimes I cannot overcome the stiffness to use my arm or leg":3, "My arm or leg is so contracted that I cannot use it at all":4},
-        "spasm_lt_leg": {"":0,"I do not have stiffness or spasms":0, "Mild, does not make it hard for me to use":1, "Moderate stiffness, but with effort I can use":2, "Sometimes I cannot overcome the stiffness to use my arm or leg":3, "My arm or leg is so contracted that I cannot use it at all":4}}
-
-def leaf_labels(obj):
-    """Get terminal node labels from a YAML/JSON-sourced dict/list object tree.
-
-    Returns only terminal node labels in DFS order.
-    """
-    def iter_dict(dc):
-        for k, v in dc.items():
-            if not v:
-                yield k
-            elif isinstance(v, dict):
-                for key in iter_dict(v):
-                    yield key
-            else:
-                for val in v:
-                    yield val
-
-    assert isinstance(obj, dict)
-    for key in iter_dict(obj):
-        yield key
+        "rt_face": {"I do not have muscle weakness in my face":0, "A little, such as when I furrow my eyebrows or laugh":1,"A lot, such as trouble with drooling or when closing my eye(s)":2,"Total weakness or palsy, such as Bell's palsy":3},
+        "lt_face": {"I do not have muscle weakness in my face":0, "A little, such as when I furrow my eyebrows or laugh":1,"A lot, such as trouble with drooling or when closing my eye(s)":2,"Total weakness or palsy, such as Bell's palsy":3},
+        "feeling_rt": {"Feeling is very good. No numbness or pain":0, "Feeling is good. Some numbness":1, "Feeling is fair. Hard to tell sharp touch from dull touch":2, "Feeling is poor. Mild pain":3, "Feeling is very poor or gone. Moderate to severe pain":4},
+        "feeling_lt": {"Feeling is very good. No numbness or pain":0, "Feeling is good. Some numbness":1, "Feeling is fair. Hard to tell sharp touch from dull touch":2, "Feeling is poor. Mild pain":3, "Feeling is very poor or gone. Moderate to severe pain":4},
+        "vision_rt": {"I have no problems with my ability to see.":0, "My vision is slightly impaired":1, "My vision is very impaired":2, "I am blind, or almost blind, in that eye":3},
+        "vision_lt": {"I have no problems with my ability to see.":0, "My vision is slightly impaired":1, "My vision is very impaired":2, "I am blind, or almost blind, in that eye":3},
+        "right_arm": {"Feeling is very good - No problems": 0, "Feeling is mildly impaired": 1, "Feeling is clearly impaired": 2, "Feeling is very poor or completely gone": 3},
+        "right_leg": {"Feeling is very good - No problems": 0, "Feeling is mildly impaired": 1, "Feeling is clearly impaired": 2, "Feeling is very poor or completely gone": 3},
+        "left_arm": {"Feeling is very good - No problems": 0, "Feeling is mildly impaired": 1, "Feeling is clearly impaired": 2, "Feeling is very poor or completely gone": 3},
+        "left_leg": {"Feeling is very good - No problems": 0, "Feeling is mildly impaired": 1, "Feeling is clearly impaired": 2, "Feeling is very poor or completely gone": 3},
+        "strength_rt_arm": {"I can easily raise it and keep it raised":0, "I can raise it, but have mild or occasional trouble with my full strength": 1, "I have to make some effort to raise it": 2, "I can barely raise it": 3, "I can move my limb, but I cannot raise it": 4, "I cannot move it at all":5},
+        "strength_lt_arm": {"I can easily raise it and keep it raised":0, "I can raise it, but have mild or occasional trouble with my full strength": 1, "I have to make some effort to raise it": 2, "I can barely raise it": 3, "I can move my limb, but I cannot raise it": 4, "I cannot move it at all":5},
+        "strength_lt_leg": {"I can easily raise it and keep it raised":0, "I can raise it, but have mild or occasional trouble with my full strength": 1, "I have to make some effort to raise it": 2, "I can barely raise it": 3, "I can move my limb, but I cannot raise it": 4, "I cannot move it at all":5},
+        "strength_rt_leg": {"I can easily raise it and keep it raised":0, "I can raise it, but have mild or occasional trouble with my full strength": 1, "I have to make some effort to raise it": 2, "I can barely raise it": 3, "I can move my limb, but I cannot raise it": 4, "I cannot move it at all":5},
+        "tremor_legs": {"I do not have tremors or coordination problems":0, "Rarely make it hard for me to use":1, "Sometimes make it hard for me to use":2, "Often make it hard for me to use":3, "Always make it hard for me to use":4},
+        "tremor_arms": {"I do not have tremors or coordination problems":0, "Rarely make it hard for me to use":1, "Sometimes make it hard for me to use":2, "Often make it hard for me to use":3, "Always make it hard for me to use":4},
+        "spasm_rt_arm": {"I do not have stiffness or spasms":0, "Mild, does not make it hard for me to use":1, "Moderate stiffness, but with effort I can use":2, "Sometimes I cannot overcome the stiffness to use my arm or leg":3, "My arm or leg is so contracted that I cannot use it at all":4},
+        "spasm_lt_arm": {"I do not have stiffness or spasms":0, "Mild, does not make it hard for me to use":1, "Moderate stiffness, but with effort I can use":2, "Sometimes I cannot overcome the stiffness to use my arm or leg":3, "My arm or leg is so contracted that I cannot use it at all":4},
+        "spasm_rt_leg": {"I do not have stiffness or spasms":0, "Mild, does not make it hard for me to use":1, "Moderate stiffness, but with effort I can use":2, "Sometimes I cannot overcome the stiffness to use my arm or leg":3, "My arm or leg is so contracted that I cannot use it at all":4},
+        "spasm_lt_leg": {"I do not have stiffness or spasms":0, "Mild, does not make it hard for me to use":1, "Moderate stiffness, but with effort I can use":2, "Sometimes I cannot overcome the stiffness to use my arm or leg":3, "My arm or leg is so contracted that I cannot use it at all":4},
+        "fatigue": {"I experience no fatigue":0,"I experience mild fatigue. I do feel the need to rest more often, but I can still complete all my daily tasks":1,"Due to my fatigue I have to rest unusually often; this affects less than half of my daily activities. I often cannot complete my daily routine without naps or significant rest.":2}}
 
 class BrainWalkData:
-    def __init__(self, row):
+    def __init__(self, row,labels,template_svg_path):
+        logging.basicConfig(level=logging.DEBUG)
         # create map with body part to dictionary of symptoms
         self.patient_id = row['DeID']
+        self.labels = labels
         # dictionary of body part -> {shade_intensity, {field_map}}
+        self.tree = ET.ElementTree(file=template_svg_path)
         self.body_parts = {}
         self.initializeBodyPartData(row)
 
     def initializeBodyPartData(self,row):
-        yaml_path = os.path.join(os.getcwd(), 'bodymap', 'maps','molemapper','molemapper.yaml')
-        with open(yaml_path) as f:
-            vocab_tree = yaml.full_load(f)
-        labels = set(leaf_labels(vocab_tree))
-
-        svg_path = os.path.join(os.getcwd(), 'bodymap', 'maps','molemapper','molemapper.plain.svg')
-        tree = ET.ElementTree(file=svg_path)
-        for element in tree.iter():
+        for element in self.tree.iter():
             id = element.get("id")
-            if id in labels:
-                bodyPart = BodyPart(id,row)
-                self.body_parts[id] = [bodyPart.shade_intensity,bodyPart.scores]
+            if id in self.labels:
+                intensity = self.parseScores(id,row,element)
+                style = ShapeStyle({"fill": intensity})
+                element.set("style", str(style))
+                self.body_parts[id] = {}
+        final_path = os.path.join(os.getcwd(),'static','patient_avatars','avatar_patient.svg')
+        self.tree.write(final_path)
   
-class BodyPart:
-    def __init__(self,name,row):
-        self.name = name
-        self.shade_intensity = ""
-        self.scores = {}
-        self.setScores(row)
-    
-    def setScores(self,row):
-        fields = BODY_PART_TO_BRAINWALK_FIELDS[self.name] if self.name in BODY_PART_TO_BRAINWALK_FIELDS.keys() else []
+    def parseScores(self,name,row,element):
+        fields = BODY_PART_TO_BRAINWALK_FIELDS[name] if name in BODY_PART_TO_BRAINWALK_FIELDS.keys() else []
         max_score = 0
+        scores = {}
         for field in fields:
             map = BRAINWALK_FIELD_STRING_TO_INT_SCORE[field] if field in BRAINWALK_FIELD_STRING_TO_INT_SCORE.keys() else {}
             value = 0
             string_entry = ""
             if field in row.keys():
                 string_entry = row[field]
-                value = map[string_entry]/(len(map.keys())-1)
+                value = map[string_entry]/(len(map.keys())-1) if string_entry != "" else 0
                 max_score = max(max_score,value)
-            self.scores[field] = string_entry
-        self.shade_intensity = self.intensity2color(max_score)
+            scores[field] = string_entry
+        return self.intensity2color(max_score)
 
     def intensity2color(self,scale):
         """Interpolate from pale green to pale pink
@@ -125,6 +97,38 @@ class BodyPart:
         new_rbg = tuple(baseline + int(round(scale * (component - baseline)))
                         for component in max_rgb)
         return "#%02x%02x%02x" % new_rbg
+
+class ShapeStyle(object):
+    base_styles = {
+        "fill": "#cccccc",
+        "fill-rule": "evenodd",
+        "stroke": "grey",
+        "stroke-width": "2px",
+        "stroke-linecap": "butt",
+        "stroke-linejoin": "miter",
+        "stroke-opacity": 1,
+    }
+
+    def __init__(self,styles):
+        styles = dict(styles)
+        # Safety dance
+        for k in styles:
+            if k not in self.base_styles:
+                raise ValueError("Unexpected style: %s" % k)
+        self._styles = self.base_styles.copy()
+        self._styles.update(styles)
+
+    def __getitem__(self, key):
+        return self._styles[key]
+
+    def __setitem__(self, key, value):
+        assert key in self._styles
+        self._styles[key] = value
+
+    def __str__(self):
+        return ';'.join("%s:%s" % (k, v)
+                        for k, v in sorted(self._styles.items()))
+
 
 
     
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+import logging
 from flask import Flask, request
 from flask import jsonify
 from flask import render_template
 from brainwalkrecord import BrainWalkData
-import matplotlib.pyplot as plt, mpld3
-from MediPlot import BodyMap
-from bodymap.demo import execute
-import svgwrite
+import matplotlib.pyplot as plt
 import os
 import csv
-
+import yaml
+import logging
 '''
 TODOs:
 1. Change RGB color scheme from blues to pale green to pale red
@@ -23,20 +22,46 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
+    app.logger.setLevel(logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
     patientId = request.args.get('patientId')
     file_name = 'avatar_data.csv'
     file_path = os.path.join(os.getcwd(), 'static', file_name)
+
+    template_path = os.path.join(os.getcwd(), 'static', 'avatar_template','avatar-template.plain.svg')
     with open(file_path, 'r',encoding="Windows-1252") as csvfile:
-        # Create a CSV reader object
         csv_reader = csv.DictReader(csvfile)
         for row in csv_reader:
-            # add logic here to get max date
             if row['DeID'] == patientId:
-                brainWalkData = BrainWalkData(row)
-                execute(brainWalkData)
+                brainWalkData = BrainWalkData(row,getLabels(),template_path)
                 return render_template('index.html',patientId=patientId,data=brainWalkData.body_parts)
-            break
         return render_template('index.html',patientId=patientId,data={})
+    
+def getLabels():
+    yaml_path = os.path.join(os.getcwd(), 'static', 'body_parts.yaml')
+    with open(yaml_path) as f:
+        vocab_tree = yaml.full_load(f)
+    return set(leaf_labels(vocab_tree))
+
+def leaf_labels(obj):
+    """Get terminal node labels from a YAML/JSON-sourced dict/list object tree.
+
+    Returns only terminal node labels in DFS order.
+    """
+    def iter_dict(dc):
+        for k, v in dc.items():
+            if not v:
+                yield k
+            elif isinstance(v, dict):
+                for key in iter_dict(v):
+                    yield key
+            else:
+                for val in v:
+                    yield val
+
+    assert isinstance(obj, dict)
+    for key in iter_dict(obj):
+        yield key
     
 if __name__ == '__main__':
     app.run(host="localhost", port=5000, debug=True)
