9ce5f66b9b892af84a5efd8bed07b5a9756e0cea
adigreg <ariannadigregorio2020@u.northwestern.edu>
2024-01-17 09:50:24 -0800

    Tooltip box with formatting works!

--- static/js/render_avatar.js
+++ static/js/render_avatar.js
@@ -1,51 +1,73 @@
 d3.xml(avatarUrl).then(function(xml) {
-        var tooltip = d3.select("body").append('div')
-            .attr('class', 'tooltip')
-            .style('position','absolute')
-            .style('opacity', 0).style('width', '500px') // Set the width of the tooltip
-            .style('height', '50px');
+    var tooltip = d3.select("body").append('div')
+        .attr('class', 'tooltip')
+        .style('position','absolute')
+        .style('opacity', 0)
+        .style('width', 'auto') // Set the width of the tooltip
+        .style('height', 'auto')
+        .style("background-color", "#fff") // Set background color
+        .style("border", "1px solid #ccc");
     d3.select("body").node().appendChild(xml.documentElement);
     pathElements = d3.selectAll("path");
     pathElements.on("mouseover", function(d) {
+            var pathElement = d3.select(this);
+            var id = pathElement.attr('id');
+            if(id != null && (id[0] == "g" || id[0] == "p")){
+                id = "brain"
+                pathElement = d3.select("#parent")
+            }
+            var formattedHtml = ""
+            for(var surveyValue in bodyPartData[id]){
+                formattedHtml += 
+                "<div><strong>" + surveyValue + "</strong>" + "</div>"
+                + "<div>" + bodyPartData[id][surveyValue] + "</div>";
+            }
+            if(formattedHtml == ""){
+                formattedHtml = "<div>No issues with " + id + "!</div>"
+            }
+            tooltip.html(formattedHtml)
+            .transition()
+            .style('opacity', 1)
+            .delay(0)
+            .style('position','absolute')
+            .style("left", (d3.event.offsetX + 10) + "px")
+            .style("top", (d3.event.offsetY + 10) + "px")
+            .style("display", "block");
+            pathElement.style('stroke','green').style('stroke-width','2px')
+    });
+    pathElements.on("mousemove", function(d) {
         var pathElement = d3.select(this);
         var id = pathElement.attr('id');
         if(id != null && (id[0] == "g" || id[0] == "p")){
             id = "brain"
             pathElement = d3.select("#parent")
         }
-    var tooltipText = JSON.stringify(bodyPartData[id])
-        tooltip.transition()
-        .style('opacity', 0.9)
-        .delay(0)
-        .style('position','absolute')
-        .style("left", (event.offsetX + 10) + "px")
-        .style("top", (event.offsetY + 10) + "px")
-        .style("display", "block")
-        .text(tooltipText);
-        pathElement.style('stroke','green').style('stroke-width','4px')
-    }).pathElements.on("mousemove", function(d) {
-        var pathElement = d3.select(this);
-        var id = pathElement.attr('id');
-        if(id != null && (id[0] == "g" || id[0] == "p")){
-            id = "brain"
-            pathElement = d3.select("#parent")
+        var formattedHtml = ""
+        for(var surveyValue in bodyPartData[id]){
+            formattedHtml += 
+            "<div><strong>" + surveyValue + "</strong>" + "</div>"
+            + "<div>" + bodyPartData[id][surveyValue] + "</div>";
         }
-        var tooltipText = JSON.stringify(bodyPartData[id])
-        tooltip.transition()
-        .style('opacity', 0.9)
+        if(formattedHtml == ""){
+            formattedHtml = "<div>No issues with " + id + "!</div>"
+        }
+        tooltip.html(formattedHtml)
+        .transition()
+        .style('opacity', 1)
         .delay(0)
         .style('position','absolute')
-        .style("left", (event.offsetX + 10) + "px")
-        .style("top", (event.offsetY + 10) + "px")
-        .style("display", "block")
-        .text(tooltipText);
-    })
-    .on("mouseout", function(d) {
+        .style("left", (d3.event.offsetX + 10) + "px")
+        .style("top", (d3.event.offsetY + 10) + "px")
+        .style("display", "block");
+    });
+    pathElements.on("mouseout", function(d) {
         var pathElement = d3.select(this);
         tooltip.transition()
         .delay(0)
         .style('position','absolute')
+        .style("left", (d3.event.offsetX + 10) + "px")
+        .style("top", (d3.event.offsetY + 10) + "px")
         .style('opacity', 0);
         pathElement.style('stroke','grey').style('stroke-width','2px');
-    })
+    });
 });
